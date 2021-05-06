8974afdca525da8a68fdd1c51c4986c321f5da3c
DevasNAI <55808186+DevasNAI@users.noreply.github.com>
2021-05-06 09:57:11 -0600

    Andrés: Código base
    Se agrega el código que será manipulado por el equipo.

---
+++ CodigoBase.py
@@ -0,0 +1,44 @@
+import cv2
+import time
+import argparse
+
+
+if __name__ == '__main__':
+    tiempoInicio = time.time()
+
+    parser = argparse.ArgumentParser(description='Camera visualization')
+
+    ### Positional arguments
+    parser.add_argument('-i', '--cameraSource', default=0, help="Introduce number or camera path, default is 0 (default cam)")
+
+    
+    args = vars(parser.parse_args())
+
+
+    cap = cv2.VideoCapture(args["cameraSource"]) #0 local o primary camera
+    while cap.isOpened():
+        
+        #BGR image feed from camera
+        success,img = cap.read()    
+        
+        if not success:
+            break
+        if img is None:
+            break
+
+        
+        cv2.imshow("Video webcam", img)
+
+        k = cv2.waitKey(10)
+        if k==27:
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+    print('Script took %f seconds.' % (time.time() - tiempoInicio))
+
+
+
