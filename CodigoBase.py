dac37a4a1e814177811b0a0390172a279443cea5
angelicaalemanc <alemancangelica1h@gmail.com>
2021-05-06 10:31:13 -0600

    Angélica_Commit4

--- CodigoBase.py
+++ CodigoBase.py
@@ -2,6 +2,19 @@ import cv2
 import time
 import argparse
 
+def transparenteOverlay(src, overlay , pos = (0,0)  , scale = 1):
+    overlay = cv2.resize(overlay , (0,0) ,fx = scale , fy = scale)
+    h, w, _ =  overlay.shape # tamaño de la imagen de primer plano
+    rows, cols , _ = src.shape  # tamaño de la imagen de fondo
+    y, x = pos[0] , pos [1]
+
+    for i in range(h):
+        for j in range(w):
+            if x + i > rows or y + j >=cols:
+                continue
+            alpha = float(overlay[i][j][3]/255) # leer el canal alfa
+            src[x+i][y+j] = alpha * overlay[i][j][:3] + (1-alpha) * src[x+i][y+j]
+    return src
 
 if __name__ == '__main__':
     tiempoInicio = time.time()
